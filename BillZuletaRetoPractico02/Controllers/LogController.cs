c741aab67245eb50ab76cac66fd2b816e1fc14e0
JrBizu <bjzuleta.92@gmail.com>
2024-10-04 22:38:53 -0500

    Create Log Campos

---
+++ BillZuletaRetoPractico02/Controllers/LogController.cs
@@ -0,0 +1,31 @@
+﻿using Application.UseCases.Logs.Commands.CreateLog;
+using Application.UseCases.Logs.Queries.GetLogs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillZuletaRetoPractico02.Controllers
+{
+    public class LogController : BaseController
+    {
+        [HttpGet]
+        [Route("GetAll")]
+        [Produces(typeof(GetLogsQueryDto))]
+        [ActionName(nameof(GetAll))]
+        public async Task<IActionResult> GetAll()
+        {
+            var query = new GetLogsQuery();
+            var result = await this.Mediator.Send(query);
+            return this.FromResult(result);
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [Produces(typeof(CreateLogCommandDto))]
+        [ActionName(nameof(Create))]
+        public async Task<IActionResult> Create(CreateLogCommandModel model)
+        {
+            var command = this.Mapper.Map<CreateLogCommand>(model);
+            var result = await this.Mediator.Send(command);
+            return this.FromResult(result);
+        }
+    }
+}
